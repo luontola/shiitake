2736bbb2d7cad7e5d55a9caa2318fc4930415ae8
Esko Luontola <esko@luontola.fi>
2023-09-27 17:49:46 +0300

    React 18

--- dev/index.jsx
+++ dev/index.jsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import Shiitake from '../src';
 
@@ -150,4 +151,6 @@ class App extends React.Component {
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('react-root'));
+const domNode = document.getElementById('react-root')
+const root = createRoot(domNode);
+root.render(<App />);
